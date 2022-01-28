156d1c511dac6ee3175a0aad5bcb8d5cda1b9360
Michael Teuscher <70554955+mfteuscher@users.noreply.github.com>
2022-01-28 15:24:07 -0700

    create more presenters

---
+++ client/app/src/main/java/edu/byu/cs/tweeter/client/presenter/StoryPresenter.java
@@ -0,0 +1,14 @@
+package edu.byu.cs.tweeter.client.presenter;
+
+public class StoryPresenter {
+
+    private View view;
+
+    public StoryPresenter(View view) {
+        this.view = view;
+    }
+
+    public interface View {
+
+    }
+}
